89e192147cbc92e2020612b0cf01224ed837170f
banhducnonggg <banhducnonggg@gmail.com>
2024-03-12 01:54:03 -0600

    Initial Commit

---
+++ PhotoParade3/PhotoParade3App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoParade3App.swift
+//  PhotoParade3
+//
+//  Created by Nam Anh Vũ on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct PhotoParade3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
